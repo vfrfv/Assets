646d9ac0d916cf3f8b688c214a47544be5e5e13e
vfrfv <valuev.12@mail.ru>
2023-10-04 13:34:48 +0300

    Initial commit

---
+++ Scripts/Siren.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class Siren : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+
+    private Coroutine _increaseCoroutine;
+    private float _increaseValue = 0.2f;
+    private float _reductionValue = -0.2f;
+    private float _value = 0;
+
+    public void Play()
+    {
+        _value = _increaseValue;
+
+        if (_increaseCoroutine == null)
+        {
+            _increaseCoroutine = StartCoroutine(SmoothlyChangeVolume());
+        }
+    }
+
+    public void Stop()
+    {
+        _value = _reductionValue;
+    }
+
+    private IEnumerator SmoothlyChangeVolume()
+    {
+        float minValue = 0f;
+
+        _audioSource.Play();
+        _audioSource.volume = 0.1f;
+
+        while (_audioSource.volume > minValue)
+        {
+            _audioSource.volume += _value;
+            yield return new WaitForSeconds(1f);
+        }
+
+        _increaseCoroutine = null;
+    }
+}
